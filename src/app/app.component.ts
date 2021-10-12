3da566f6cddeb676ecaa79892ddd364ea6387166
Will 保哥 <doggy.huang@gmail.com>
2021-10-12 11:01:21 +0800

    使用 Subject 搭配 takeUntil 實作自動退訂功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, BehaviorSubject, timer } from 'rxjs';
-import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, BehaviorSubject, timer, Subject } from 'rxjs';
+import { map, shareReplay, switchMap, takeUntil } from 'rxjs/operators';
 
 
 @Component({
@@ -9,13 +9,15 @@ import { map, shareReplay, switchMap } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'demo1';
 
+  destroy$ = new Subject();
   refresh$ = new BehaviorSubject(null);
   data$: Observable<any> = of([]);
   time$ = timer(500, 1000).pipe(
-    map((v, idx) => new Date().toString())
+    map((v, idx) => new Date().toString()),
+    takeUntil(this.destroy$)
   );
 
   constructor(private http: HttpClient) {}
@@ -23,8 +25,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.data$ = this.refresh$.pipe(
       switchMap(() => this.http.get<any>('/assets/data.json')),
-      shareReplay(1)
+      shareReplay(1),
+      takeUntil(this.destroy$)
     );
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   refresh() {
