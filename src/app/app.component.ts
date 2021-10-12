dbad96427b5402ae2adb5c1122dc006d648b4a1c
Will 保哥 <doggy.huang@gmail.com>
2021-10-12 10:07:00 +0800

    使用 HttpClient 讀取內容並用 shareReplay 快取最後一筆結果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { shareReplay, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo1';
+  data$: Observable<any> = of([]);
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.data$ = this.http.get<any>('/assets/data.json')
+      .pipe(
+        shareReplay(1)
+      );
+  }
 }
